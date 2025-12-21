47fd8bba79dcc0e6e6a8e35416da55528951c464
현빈 <hhb041127@naver.com>
2025-12-22 05:54:51 +0900

    feat: input library

---
+++ lib/todo/input/index.ts
@@ -0,0 +1,26 @@
+import { todoStore } from "../../../store/todo-store.ts";
+
+export class Input {
+  private element: HTMLInputElement;
+
+  constructor(element: HTMLInputElement) {
+    this.element = element;
+  }
+
+  add() {
+    const value = this.element.value;
+
+    if (!value) {
+      alert("내용을 입력해주세요");
+      return;
+    }
+
+    todoStore.add({
+      id: todoStore.getId(),
+      content: value,
+      isDone: false,
+    });
+
+    this.element.value = "";
+  }
+}
